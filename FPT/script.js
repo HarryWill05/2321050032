d94b0b907bbc1eae7fd2b2cfb38579b8b7bc3bc1
HarryWill05 <huydaothe835@gmail.com>
2025-11-03 18:21:22 +0700

    FPT

--- FPT/script.js
+++ FPT/script.js
@@ -16,7 +16,7 @@ let danhSachPhim = [
         tuoi: 13,
         thoiLuong: 1.5,
         quocGia: "Nhật Bản",
-           poster: "url('./img/poster/conan_27.jpg')"
+        poster: "url('./img/poster/conan_27.jpg')"
 
     }
 ]
@@ -28,7 +28,14 @@ let banner = document.getElementsByClassName('banner')[0];
 function chonPhim(idPhim){
     for (let i = 0; i < danhSachPhim.length; i++){
         if(danhSachPhim[i].id == idPhim){
+            
             banner.style.backgroundImage = danhSachPhim[i].poster;
+            
+            document.getElementById('tenPhim').innerHTML = danhSachPhim[i].tenPhim;
+            document.getElementById('namPhatHanh').innerHTML = danhSachPhim[i].namPhatHanh;
+            document.getElementById('thoiLuong').innerHTML = danhSachPhim[i].thoiLuong;
+            document.getElementById('tuoi').innerHTML = danhSachPhim[i].tuoi;
+            document.getElementById('quocGia').innerHTML = danhSachPhim[i].quocGia;
         }
     }
 }
