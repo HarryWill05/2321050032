d8321330efb7ffd601d15e121e847c477b4d0450
HarryWill05 <huydaothe835@gmail.com>
2025-11-03 17:45:09 +0700

    temp

---
+++ FPT/script.js
@@ -0,0 +1,34 @@
+let danhSachPhim = [
+    {
+        id: 1,
+        tenPhim: "Mưa đỏ",
+        namPhatHanh: 2025,
+        tuoi: 16,
+        thoiLuong: 2,
+        quocGia: "Việt Nam",
+        poster: "url('./img/mua-do2-1122.jpeg')"
+    },
+
+    {
+        id: 2,
+        tenPhim: "Conan",
+        namPhatHanh: 2024,
+        tuoi: 13,
+        thoiLuong: 1.5,
+        quocGia: "Nhật Bản",
+           poster: "url('./img/poster/conan_27.jpg')"
+
+    }
+]
+
+let phimHienTai = danhSachPhim[0];
+
+let banner = document.getElementsByClassName('banner')[0];
+
+function chonPhim(idPhim){
+    for (let i = 0; i < danhSachPhim.length; i++){
+        if(danhSachPhim[i].id == idPhim){
+            banner.style.backgroundImage = danhSachPhim[i].poster;
+        }
+    }
+}
